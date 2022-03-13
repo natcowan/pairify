dd7d8a9da0eaf2f4d0201c237664eaa6df33792d
Natalie Cowan <nataliecowan011@gmail.com>
2022-03-13 13:14:53 -0700

    Production value = true

--- app/app.py
+++ app/app.py
@@ -32,7 +32,7 @@ MATCH_PLAYLIST_ID = '10eJ5YNR0xdDUUgZkx48MP'
 Data is stored as a tuple, with the first element being the state and the second
 the date the state was created. '''
 STATES = {}
-PRODUCTION = True if 'PRODUCTION' in os.environ and os.environ['PRODUCTION'].lower() == 'true' else False
+PRODUCTION = True
 
 PRODUCTION = False
 if PRODUCTION:
